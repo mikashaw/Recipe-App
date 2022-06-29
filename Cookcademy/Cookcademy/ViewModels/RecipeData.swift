e8c349bc940d0db97db9d231309da3c60471001d
Mika Shaw <mikashaw2003@gmail.com>
2022-06-29 12:18:26 -0400

    added add recipe functionality

--- Cookcademy/Cookcademy/ViewModels/RecipeData.swift
+++ Cookcademy/Cookcademy/ViewModels/RecipeData.swift
@@ -20,4 +20,10 @@ class RecipeData: ObservableObject {
         return filteredRecipes
     }
     
+    func add(recipe: Recipe){
+        if recipe.isValid {
+            recipes.append(recipe)
+        }
+    }
+    
 }
